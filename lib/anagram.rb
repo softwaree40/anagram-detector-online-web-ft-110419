130880d62b107c927af807550a7fe3030a789581
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-19 23:35:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require "pry"
 # Your code goes here!
+class Anagram
+       attr_accessor:word
+   
+       def initialize (word)
+           @word = word
+           #binding.pry
+      end
+      def match(words)
+          words.collect do |each_word|
+            if each_word == word
+             
+            end
+            #binding.pry
+         end
+          
+      end
+end
